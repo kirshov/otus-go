2b8cd5e5cb0758220a7c76e0a1034d2aa86e0e28
leonid <kirshov@gmail.com>
2025-03-23 01:57:01 +0500

    hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,70 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+const listSize = 10
+
+var re = regexp.MustCompile(`[\p{L}-]+|-`)
+
+type wordsCount struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	words := re.FindAllString(strings.ToLower(s), -1)
+	list := make(map[string]int)
+
+	for _, word := range words {
+		list[word]++
+	}
+
+	return getSortTopList(list)
+}
+
+func getSortTopList(list map[string]int) []string {
+	sl := createWordsCountSlice(list)
+	sl = sortList(sl)
+
+	maxSize := listSize
+	if len(sl) < listSize {
+		maxSize = len(sl)
+	}
+
+	r := make([]string, 0, maxSize)
+	for _, item := range sl[:maxSize] {
+		r = append(r, item.word)
+	}
+
+	return r
+}
+
+func createWordsCountSlice(list map[string]int) []wordsCount {
+	sl := make([]wordsCount, 0, len(list))
+
+	for w, c := range list {
+		if w == "-" {
+			continue
+		}
+
+		sl = append(sl, wordsCount{w, c})
+	}
+
+	return sl
+}
+
+func sortList(sl []wordsCount) []wordsCount {
+	sort.Slice(sl, func(i, j int) bool {
+		if sl[i].count == sl[j].count {
+			return sl[i].word < sl[j].word
+		}
+
+		return sl[i].count > sl[j].count
+	})
+
+	return sl
 }
