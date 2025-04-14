33ecb7778698e4a50ccc20d0c8509e027eae48c4
leonid <kirshov@gmail.com>
2025-04-15 01:31:08 +0300

    hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/schollz/progressbar/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,55 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset >= fInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fSource, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fSource.Close()
+
+	tTarget, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer tTarget.Close()
+
+	if limit == 0 {
+		limit = fInfo.Size()
+	}
+	if offset > 0 {
+		_, err = fSource.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	reader := io.LimitReader(fSource, limit)
+
+	if ShowProgress {
+		err = CopyWithProgress(tTarget, reader, fInfo.Size())
+	} else {
+		_, err = io.Copy(tTarget, reader)
+	}
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func CopyWithProgress(writer io.Writer, reader io.Reader, total int64) error {
+	bar := progressbar.NewOptions64(total)
+	_, err := io.Copy(io.MultiWriter(writer, bar), reader)
+
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type item struct {
+		compare string
+		offset  int64
+		limit   int64
+	}
+	l := []item{
+		{compare: "testdata/out_offset0_limit0.txt"},
+		{compare: "testdata/out_offset0_limit10.txt", limit: 10},
+		{compare: "testdata/out_offset0_limit1000.txt", limit: 1000},
+		{compare: "testdata/out_offset0_limit10000.txt", limit: 10000},
+		{compare: "testdata/out_offset100_limit1000.txt", limit: 1000, offset: 100},
+		{compare: "testdata/out_offset6000_limit1000.txt", limit: 1000, offset: 6000},
+	}
+
+	for _, v := range l {
+		t.Run(v.compare, func(t *testing.T) {
+			err := Copy("testdata/input.txt", "testdata/out.txt", v.offset, v.limit)
+			require.NoError(t, err)
+
+			sf, err := os.ReadFile("testdata/out.txt")
+			require.NoError(t, err)
+
+			tf, err := os.ReadFile(v.compare)
+			require.NoError(t, err)
+			require.Equal(t, sf, tf)
+
+			_ = os.Remove("testdata/out.txt")
+		})
+	}
 }
